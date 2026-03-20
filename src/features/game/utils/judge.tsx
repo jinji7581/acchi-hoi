1c8e7ec139035fc4621f3b51b29d9b49e3c7454b
jinji7581 <con14560@ict.nitech.ac.jp>
2026-03-20 20:58:29 +0900

    判定機能の骨ぐみ完成

--- src/features/game/utils/judge.tsx
+++ src/features/game/utils/judge.tsx
@@ -1,6 +1,29 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useEffect } from "react";
+// import {GameState} from "../../../zzz_globalVar"
 export const Judge: React.FC = () => {
+  type Direction = "up" | "down" | "left" | "right" | "center" | null;
+
+  const currentDirections: Direction[] = ["right", "center", "up", "down"]; //仮データ
+  const playerDirections: Direction[] = ["right", "right", "down", "up"];
+  const isPointSystem: boolean = true;
+
+  useEffect(() => {
+    let counter: number = 0;
+    for (const element of playerDirections) {
+      for (const element2 of currentDirections) {
+        if (element === element2) break;
+        counter = counter + 1;
+      }
+      if (counter === currentDirections.length) {
+        //プレイヤーのポイント増加++
+      } else {
+        if (!isPointSystem) {
+          //プレイヤーの残機--
+        }
+      }
+    }
+  }, []);
+
   return <div></div>;
 };
 
